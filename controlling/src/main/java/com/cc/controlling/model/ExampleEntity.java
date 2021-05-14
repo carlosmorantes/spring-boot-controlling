dcd1a529f31cdbba5d72606c3796ccf9802d5ac2
carlosmorantes <carlostenis20@hotmail.com>
2021-05-14 18:16:52 +0200

    Add email attribute to entity and view

--- controlling/src/main/java/com/cc/controlling/model/ExampleEntity.java
+++ controlling/src/main/java/com/cc/controlling/model/ExampleEntity.java
@@ -5,6 +5,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -21,6 +22,9 @@ public class ExampleEntity {
 
     @NotNull(message = "Name must not be null.")
     private String name;
+
+    @Email(message = "EMail must be valid.")
+    private String email;
 
     public long getId() {
 	return id;
@@ -44,6 +48,14 @@ public class ExampleEntity {
 
     public void setName(String name) {
 	this.name = name;
+    }
+
+    public String getEmail() {
+	return email;
+    }
+
+    public void setEmail(String email) {
+	this.email = email;
     }
 
 }
