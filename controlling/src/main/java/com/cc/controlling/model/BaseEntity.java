a9982b8681c001324c67b282f2290f2a6097b0e2
carlosmorantes <carlostenis20@hotmail.com>
2021-05-20 14:55:41 +0200

    Add base entity

---
+++ controlling/src/main/java/com/cc/controlling/model/BaseEntity.java
@@ -0,0 +1,86 @@
+package com.cc.controlling.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotBlank(message = "Name must must not be null.")
+    @Size(min = 2, max = 50, message = "Name must have at least 2 characters and maximum 50.")
+    private String name;
+
+    private String description;
+
+    public long getId() {
+	return id;
+    }
+
+    public void setId(long id) {
+	this.id = id;
+    }
+
+    public String getName() {
+	return name;
+    }
+
+    public void setName(String name) {
+	this.name = name;
+    }
+
+    public String getDescription() {
+	return description;
+    }
+
+    public void setDescription(String description) {
+	this.description = description;
+    }
+
+    @Override
+    public String toString() {
+	return "BaseEntity [id=" + id + ", name=" + name + ", description=" + description + "]";
+    }
+
+    @Override
+    public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + ((description == null) ? 0 : description.hashCode());
+	result = prime * result + (int) (id ^ (id >>> 32));
+	result = prime * result + ((name == null) ? 0 : name.hashCode());
+	return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj)
+	    return true;
+	if (obj == null)
+	    return false;
+	if (getClass() != obj.getClass())
+	    return false;
+	BaseEntity other = (BaseEntity) obj;
+	if (description == null) {
+	    if (other.description != null)
+		return false;
+	} else if (!description.equals(other.description))
+	    return false;
+	if (id != other.id)
+	    return false;
+	if (name == null) {
+	    if (other.name != null)
+		return false;
+	} else if (!name.equals(other.name))
+	    return false;
+	return true;
+    }
+
+}
